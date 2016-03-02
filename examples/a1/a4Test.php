af7751e12e09167f53d77494fddde20fdd0cf27c
a.nechaev <a.nechaev@b2b-center.ru>
2016-03-03 00:08:45 +0300

    change tests

--- examples/a1/a4Test.php
+++ examples/a1/a4Test.php
@@ -1,8 +1,23 @@
 <?php
 
-include_once '../BaseTest.php';
+class a4Test extends PHPUnit_Framework_TestCase {
 
-class a4Test extends BaseTest {
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testSleep($time) {
+        sleep($time);
+        $this->assertTrue((bool) rand(0,1));
+    }
 
+    public function additionProvider() {
+        $arr = [];
+        for ($i = 0; $i < rand(5, 10); $i++) {
+            $arr[$i][] = rand(0, 1);
+        }
+
+
+        return $arr;
+    }
 
 }
--- examples/b1/b1Test.php
+++ examples/b1/b1Test.php
@@ -1,8 +1,23 @@
 <?php
 
-include_once '../BaseTest.php';
+class b1Test extends PHPUnit_Framework_TestCase {
 
-class b1Test extends BaseTest {
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testSleep($time) {
+        sleep($time);
+        $this->assertTrue((bool) rand(0,1));
+    }
 
+    public function additionProvider() {
+        $arr = [];
+        for ($i = 0; $i < rand(5, 10); $i++) {
+            $arr[$i][] = rand(0, 1);
+        }
+
+
+        return $arr;
+    }
 
 }
