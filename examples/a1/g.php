6a4236f3ff75199eb261b9efb0d9ba4932e9d589
r.schekin <r.schekin@b2b-center.ru>
2016-03-04 17:03:56 +0300

    Новые тесты (Фибоначчи, обновление #4)

--- examples/a1/g.php
+++ examples/a1/g.php
@@ -1,9 +1,9 @@
 <?php
 
-$f = file_get_contents('a1Fib34Test.php');
+$f = file_get_contents('a1FibTest.php');
 
 for ($i = 2; $i < 50; $i++) {
-    $f2 = str_replace('a1Fib34Test', 'a'.$i.'Fib34Test', $f);
-    unlink('a'.$i.'Fib34Test.php');
-    file_put_contents('a'.$i.'Fib34Test.php', $f2);
+    $f2 = str_replace('a1FibTest', 'a'.$i.'FibTest', $f);
+    unlink('a'.$i.'FibTest.php');
+    file_put_contents('a'.$i.'FibTest.php', $f2);
 }
